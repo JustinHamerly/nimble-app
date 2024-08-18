94b91469dfa8f2ccbd61ff644c333753f7efc2f9
Justin <suvimountain@icloud.com>
2024-08-18 13:20:39 -0700

    added race info file for race information

---
+++ src/components/Main/CharacterCreator/BioInfoPreview.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function BioInfoPreview () {
+    return (
+        <div id='bio-info-preview-box' className="flavor-box">
+            <div id="race-info">
+
+            </div>
+            <div id="class-info">
+                
+            </div>
+        </div>
+    )
+};
+
+export default BioInfoPreview;
