3ce1a158fd42f8022f89408ac2555335d6aefeea
Justin <suvimountain@icloud.com>
2024-08-21 23:57:59 -0700

    added health tracker

---
+++ src/components/Main/CharacterSheet/HealthTracker/HeathTracker.tsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import './HealthTracker.css'
+import { SelectedCharacterContext } from '../../../../context/SelectedCharacterContext';
+
+function HealthTracker() {
+    const charContext = useContext(SelectedCharacterContext);
+    if (!charContext) return null;
+
+    return (
+        <section id="health-tracker-section">
+            <div id="health-tracker">
+                <h3>HEALTH</h3>
+                <div className='display-block'>
+                    <h4 className="value">{charContext.state.mechanicalStats.currentHP}</h4> <h4>/</h4> <h4 className='value'>{charContext.state.mechanicalStats.maxHP}</h4>
+                </div>
+            </div>
+            <div id="temp-hp-tracker">
+                <h3>TEMP HP</h3>
+                <div className='display-block' id="display-block-temp">
+                    <h4 className='value'>{charContext.state.mechanicalStats.tempHP}</h4>
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default HealthTracker;
